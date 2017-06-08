434c00d70c68ad4342438e76fd6de8ca995d64ce
0x80ff <0x80ff@protonmail.com>
2017-06-08 10:20:27 +0200

    Corrected Heuristics for Hierarchical Offline Scheduler

--- Scheduling/tools/heuristics.adb
+++ Scheduling/tools/heuristics.adb
@@ -30,58 +30,33 @@ package body Heuristics is
   --------------------------
 
   function Can_Run (This : in out Heuristic_2; Si : in Scheduling_Information) return Boolean is
-    Number_Of_Tasks_Ended : Tasks_Range := 0;
-    Task_Still_Active     : Tasks_Range := 0;
-    Safe_Units            : constant Natural := 2;
   begin
-    for num_task in 0 .. Si.Number_Of_Tasks - 1 loop
-      if Si.Tcbs(num_task).Wake_Up_Time > This.Current_Time and
-         Si.Tcbs(num_task).Rest_Of_Capacity = Si.Tcbs(num_task).Tsk.capacity
-      then
-        Number_Of_Tasks_Ended := Number_Of_Tasks_Ended + 1;
-      else
-        Task_Still_Active := num_task;
-      end if;
-    end loop;
-
-    if Number_Of_Tasks_Ended = Si.Number_Of_Tasks - 1 then
-      if Si.Tcbs(Task_Still_Active).Rest_Of_Capacity > 
-         Si.Tcbs(Task_Still_Active).Tsk.capacity - Safe_Units
-      then
-        This.Active_Task_Safety_Unit := Si.Tcbs(Task_Still_Active).Tsk.capacity - Safe_Units;
-      end if;
-
-      if Si.Tcbs(Task_Still_Active).Wake_Up_Time <= This.Current_Time and
-         Si.Tcbs(Task_Still_Active).Rest_Of_Capacity <= This.Active_Task_Safety_Unit
-      then
-        return True;
-      end if;
-    end if;
-    return False;
+    Put_Debug("__DEBUG__ :: Can_Run -> Heuristic_2 [Not Implemented]");
+    return True;
   end Can_Run;
 
-  ------------------------------------
-  -- Heuristic_1::Update_Idle_Times --
-  ------------------------------------
+  -------------------------
+  -- Heuristic_1::Update --
+  -------------------------
 
-  procedure Update_Idle_Times (This : in out Heuristic_1; No_Task : in Boolean; Core_Id : in Natural) is
+  procedure Update_Values (This : in out Heuristic_1; No_Task : in Boolean; Core_Id : in Natural) is
   begin
     if No_Task then
       Increase_Idle_Times (This, Core_Id);
     else
       Decrease_Idle_Times (This, Core_Id);
     end if;
-  end Update_Idle_Times;
+  end Update_Values;
 
-  -----------------------------------
-  -- Heuristic_1::Reset_Idle_Times --
-  -----------------------------------
+  -------------------------------
+  -- Heuristic_1::Reset_Values --
+  -------------------------------
 
-  procedure Reset_Idle_Times (This : in out Heuristic_1; Core_Id : in Natural) is
+  procedure Reset_Values (This : in out Heuristic_1; Core_Id : in Natural) is
   begin
     This.CNTI(Natural(core_id)) := 0;
     This.Number_Of_Valid_Idle_Times := 0;
-  end Reset_Idle_Times;
+  end Reset_Values;
 
   --------------------------------------
   -- Heuristic_1::Decrease_Idle_Times --
@@ -128,9 +103,20 @@ package body Heuristics is
   ----------------------------
 
   function Calculate (This : in out Heuristic_2; Si : in Scheduling_Information) return Natural is
+    Jump_Time : Natural;
+    Nearest_Wake_Up_Time : Natural;
   begin
-    Put_Debug("=== IN THE VOID :: " & This.Name);
-    return 1000000;
+    Jump_Time := This.Current_Time + Si.Tcbs(This.Remaining_Task_Id).Rest_Of_Capacity;
+    Nearest_Wake_Up_Time := Get_Nearest_Wake_Up_Time(This, Si);
+
+    Si.Tcbs(This.Remaining_Task_Id).Rest_Of_Capacity := Jump_Time - Nearest_Wake_Up_Time + 1;
+    Si.Tcbs(This.Remaining_Task_Id).Used_Capacity := Si.Tcbs(This.Remaining_Task_Id).Tsk.capacity - (Jump_Time - Nearest_Wake_Up_Time) - 1;
+
+    if Nearest_Wake_Up_Time < Jump_Time then
+      Jump_Time := Nearest_Wake_Up_Time;
+    end if;
+
+    return Jump_Time;
   end Calculate;
 
   -- procedure TestCalculation(Heuristic : Concrete_Heuristics'Class) is
@@ -138,6 +124,116 @@ package body Heuristics is
   --     Put_Debug( "Testing an heuristic" );
   --     Put_Debug( Calculate(Heuristic)'Img' );
   -- end TestCalculation;
+
+  ----------------------------------------
+  -- Heuristic_2::Verify_Pre_Conditions --
+  ----------------------------------------
+
+  function Verify_Pre_Conditions (
+    This              : in out Heuristic_2;
+    Si                : in     Scheduling_Information)
+  return Boolean is
+  begin
+    -- Test number of remaining jobs:
+    if not Get_Remaining_Task(This, Si, This.Current_Time) then
+      return False;
+    end if;
+
+    if (Si.Tcbs(This.Remaining_Task_Id).Rest_Of_Capacity > Si.Tcbs(This.Remaining_Task_Id).Tsk.capacity - 2) then
+      This.Commit_Unit := Si.Tcbs(This.Remaining_Task_Id).Tsk.capacity - 2;
+    end if;
+
+    -- Test the capacity of the remaining job:
+    if Si.Tcbs(This.Remaining_Task_Id).Tsk.capacity <= 2 then
+      return False;
+    end if;
+
+    -- Test the starting event commit:
+    if Si.Tcbs(This.Remaining_Task_Id).Wake_Up_Time > This.Current_Time
+    or Si.Tcbs(This.Remaining_Task_Id).Rest_Of_Capacity >= This.Commit_Unit
+    then
+      return False;
+    end if;
+
+    -- Test the nearest wake up time:
+    if (Get_Nearest_Wake_Up_Time(This, Si) - This.Current_Time) <= 2
+    then
+      return False;
+    end if;
+
+    -- Test the jump time:
+    if ((This.Current_Time + Si.Tcbs(This.Remaining_Task_Id).Rest_Of_Capacity) - This.Current_Time) <= 2 then
+      return False;
+    end if;
+
+    return True;
+  end Verify_Pre_Conditions;
+
+  -------------------------------------
+  -- Heuristic_2::Get_Remaining_Task --
+  -------------------------------------
+
+  function Get_Remaining_Task (
+    This         : in out Heuristic_2;
+    Si           : in     Scheduling_Information;
+    Current_Time : in     Natural)
+  return Boolean is
+  begin
+    This.Number_Of_Tasks_Ended := 0;
+    for num_task in 0 .. Si.Number_Of_Tasks - 1 loop
+      if (Si.Tcbs(num_task).Wake_Up_Time > Current_Time) and 
+         (Si.Tcbs(num_task).Rest_Of_Capacity = Si.Tcbs(num_task).Tsk.capacity)
+      then
+        This.Number_Of_Tasks_Ended := This.Number_Of_Tasks_Ended + 1;
+      else
+        This.Remaining_Task_Id := num_task;
+      end if;
+    end loop;
+
+    if This.Number_Of_Tasks_Ended = Natural(Si.Number_Of_Tasks) - 1 then
+      return True;
+    end if;
+
+    return False;
+  end Get_Remaining_Task;
+
+  -------------------------------------------
+  -- Heuristic_2::Get_Nearest_Wake_Up_Time --
+  -------------------------------------------
+
+  function Get_Nearest_Wake_Up_Time (
+    This              : in out Heuristic_2;
+    Si                : in Scheduling_Information)
+  return Natural is
+    Nearest_Wake_Up_Time : Natural := This.Current_Time + Si.Tcbs(This.Remaining_Task_Id).Rest_Of_Capacity;
+  begin
+    for num_task in 0 .. Si.Number_Of_Tasks - 1 loop
+      if Si.Tcbs(num_task).Tsk.name /= Si.Tcbs(This.Remaining_Task_Id).Tsk.name then
+        if Si.Tcbs(num_task).Wake_Up_Time < Nearest_Wake_Up_Time then
+            Nearest_Wake_Up_Time := Si.Tcbs(num_task).Wake_Up_Time;
+        end if;
+      end if;
+    end loop;
+
+    return Nearest_Wake_Up_Time;
+  end Get_Nearest_Wake_Up_Time;
+
+  -------------------------------
+  -- Heuristic_2::Reset_Values --
+  -------------------------------
+
+  procedure Reset_Values (This : in out Heuristic_2; Si : in Scheduling_Information) is
+  begin
+    if (Si.Tcbs(This.Remaining_Task_Id).Rest_Of_Capacity > 1) then
+      This.Commit_Unit := Si.Tcbs(This.Remaining_Task_Id).Rest_Of_Capacity - 2;
+    else
+      This.Commit_Unit := 0;
+      Put_Debug("INFO SAFETY PUT TO ZERO");
+    end if;
+
+    Heuristic_2(Last_Job_Heuristic.all).Remaining_Task_Id     := 0;
+    Heuristic_2(Last_Job_Heuristic.all).Number_Of_Tasks_Ended := 0;
+  end Reset_Values;
 
   -----------------------
   -- Can_Run_Heuristic --
--- Scheduling/tools/heuristics.ads
+++ Scheduling/tools/heuristics.ads
@@ -36,31 +36,40 @@ package Heuristics is
     with record
       CNTI : Core_No_Task_Info := (others => 0);
       Number_Of_Valid_Idle_Times : Natural := 0;
+      Current_Time : Natural := 0;
     end record;
 
   type Heuristic_2 is new Concrete_Heuristics
     with record
-      Current_Time            : Natural;
-      Task_Id_Still_Active    : Tasks_Range;
-      Active_Task_Safety_Unit : Natural;
+      Commit_Unit : Natural := 0;
+      Current_Time : Natural := 0;
+      Remaining_Task_Id : Tasks_Range := 0;
+      Number_Of_Tasks_Ended : Natural := 0;
     end record;
 
 
 ---------------------- METHODS -------------------------------------------------
   -- Heuristics functions and procedures:
-  function Can_Run(This : in out Heuristic_1; Si : in Scheduling_Information) return Boolean;
-  function Can_Run(This : in out Heuristic_2; Si : in Scheduling_Information) return Boolean;
+  function Can_Run (This : in out Heuristic_1; Si : in Scheduling_Information) return Boolean;
+  function Can_Run (This : in out Heuristic_2; Si : in Scheduling_Information) return Boolean;
 
-  function Calculate(This : in out Heuristic_1; Si : in Scheduling_Information) return Natural;
-  function Calculate(This : in out Heuristic_2; Si : in Scheduling_Information) return Natural;
+  function Calculate (This : in out Heuristic_1; Si : in Scheduling_Information) return Natural;
+  function Calculate (This : in out Heuristic_2; Si : in Scheduling_Information) return Natural;
 
   -- Proper to Heuristic_1::Idle_Heuristic:
-  procedure Update_Idle_Times   (This : in out Heuristic_1; No_Task : in Boolean; Core_Id : in Natural);
-  procedure Reset_Idle_Times    (This : in out Heuristic_1; Core_Id : in Natural);
+  procedure Update_Values   (This : in out Heuristic_1; No_Task : in Boolean; Core_Id : in Natural);
+  procedure Reset_Values    (This : in out Heuristic_1; Core_Id : in Natural);
+  --
   procedure Decrease_Idle_Times (This : in out Heuristic_1; Core_Id : in Natural);
   procedure Increase_Idle_Times (This : in out Heuristic_1; Core_Id : in Natural);
 
 
+  -- Proper to Heuristic_2::Last_Job_Remaining_Heuristic:
+  function  Verify_Pre_Conditions   (This : in out Heuristic_2; Si : in Scheduling_Information) return Boolean;
+  procedure Reset_Values            (This : in out Heuristic_2; Si : in Scheduling_Information);
+  --
+  function Get_Remaining_Task       (This : in out Heuristic_2; Si : in Scheduling_Information; Current_Time : in Natural) return Boolean;
+  function Get_Nearest_Wake_Up_Time (This : in out Heuristic_2; Si : in Scheduling_Information) return Natural;
 ---------------------- HEURISTICS LIST -----------------------------------------
   -- Heuristic list:
   package Heuristic_Lists is new Ada.Containers.Doubly_Linked_Lists (Heuristic_Ptr);
@@ -85,6 +94,6 @@ package Heuristics is
 
 ---------------------- HEURISTICS DECLARATIONS ---------------------------------
   -- Heuristics declarations:
-  Idle_Heuristic : Heuristic_Ptr := new Heuristic_1;
-  -- HR_2 : Heuristic_Ptr := new Heuristic_2;
+  Idle_Heuristic     : Heuristic_Ptr := new Heuristic_1;
+  Last_Job_Heuristic : Heuristic_Ptr := new Heuristic_2;
 end Heuristics;
